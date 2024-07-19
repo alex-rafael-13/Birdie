1f1bdc9fa323d7715d8dc9c28c33cf77431b17c5
alex-rafael-13 <rafael.alex1398@gmail.com>
2024-07-18 17:45:30 -0700

    user creation api working

--- myapp/serializers/auth_serializers.py
+++ myapp/serializers/auth_serializers.py
@@ -6,7 +6,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     '''
         Serializer to register a new User
     '''
-    
+
     #Make password write_only
     password = serializers.CharField(write_only=True)
 
@@ -26,8 +26,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     # Creates new user
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data["first_name"],
-            username=validated_data["last_name"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
             username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"]
