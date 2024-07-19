55ba2c230393694ad1ed7559dbb6197d4768eb01
alex-rafael-13 <rafael.alex1398@gmail.com>
2024-07-18 19:36:18 -0700

    fixed validators

--- myapp/serializers/auth_serializers.py
+++ myapp/serializers/auth_serializers.py
@@ -15,14 +15,34 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['first_name','last_name','username', 'password', 'email']
 
+    def validate(self, data):
+        '''
+            Custom Validators:
+                - first_name: Required
+                - last_name: Required
+                - email: required
+            All other fields are required by User model by default
+        '''
+        required_fields = ['first_name', 'last_name', 'email']
+
+        # Iterate through required_fields list to check if each field is filled in
+        for field in required_fields:
+
+            # If field not filled, raise ValidationError
+            if not data.get(field):
+                raise serializers.ValidationError({field: f"{field.replace('_', ' ').capitalize()} is required."})
+        
+        return data
+
+
     # Checks for unique emails
     def validate_email(self, value):
         # If user, raise validation error
-        if User.objects.filter(email=value):
-            serializers.ValidationError("This email address is already being used")
+        if User.objects.filter(email=value).exists():
+           raise serializers.ValidationError("This email address is already being used")
         # Return value if no errors
         return value
-        
+    
     # Creates new user
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -36,5 +56,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         # If successful, return user 
         return user
     
+
+class LoginSerializer(serializers.ModelSerializer):
+    '''
+        Serializer for user login
+    '''
+    pass
 
 
