cd33a4a4b77495f12153d01e6dd92e3c2d49a9cb
alex-rafael-13 <rafael.alex1398@gmail.com>
2024-07-18 17:03:18 -0700

    register user view created

--- myapp/serializers/auth_serializers.py
+++ myapp/serializers/auth_serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User #Using Django User Model
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     '''
         Serializer to register a new User
     '''
 
+    #Make password write_only
+    password = serializers.CharField(write_only=True)
+
+    # Necessary fields for user creation
     class Meta:
         model = User
         fields = ['first_name','last_name','username', 'password', 'email']
--- myapp/views/user_views.py
+++ myapp/views/user_views.py
@@ -1,4 +1,30 @@
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from ..serializers import RegisterSerializer
 
 def get_user(request):
     return HttpResponse("Hello from Users API")
+
+class RegisterView(APIView):
+    '''
+        View to register a new User
+    '''
+
+    def post(self, request):
+        # Call in register serialize
+        serializer = RegisterSerializer(data=request.data)
+
+        # If successful request data, save to database and return with 201 
+        if serializer.is_valid():
+            serializer.save()           
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # Return 400 error if unsuccesful
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
